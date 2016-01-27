08f39de0c64b50b538c0442c5afec70e9b627309
Afdal OULOUBE <aouloube@e1r6p7.42.fr>
2016-01-27 18:28:34 +0100

    update

--- 42_conf.sh
+++ 42_conf.sh
@@ -1,29 +1,71 @@
 #! /bin/bash
 
-echo "\033[33;34m |*********************************************| \033[33;0m"
-echo "\033[33;34m |*            42 mac conf by Afxsoft         *| \033[33;0m"
-echo "\033[33;34m |*********************************************| \033[33;0m"
-curl -L http://install.ohmyz.sh &> /dev/null | sh &> /dev/null
-echo "\033[33;34m |*            - Oh my Zsh             OK [✓] *| \033[33;0m"
-echo "\033[33;34m |*                                           *| \033[33;0m"
-sed -i.bu 's/ZSH_THEME="robbyrussell"/ZSH_THEME="af-magic"/' ~/.zshrc
-echo "\033[33;34m |*            - Theme af-magic        OK [✓] *| \033[33;0m"
-echo "\033[33;34m |*                                           *| \033[33;0m"
-curl https://raw.githubusercontent.com/reversTeam/Sublivim/master/installer.sh &> /dev/null | sh &> /dev/null
-echo "\033[33;34m |*            - Sublivim              OK [✓] *| \033[33;0m"
-echo "\033[33;34m |*                                           *| \033[33;0m"
-rm -rf /tmp/vim_theme/
-git clone https://github.com/chriskempson/vim-tomorrow-theme.git /tmp/vim_theme &> /dev/null
-cp /tmp/vim_theme/colors/Tomorrow-Night.vim ~/.vim/colors/.
-sed -i.bu 's/colorscheme molokai/colorscheme Tomorrow-Night/' ~/.vimrc
-echo "\033[33;34m |*            - Theme Tomorrow-Night  OK [✓] *| \033[33;0m"
-echo "\033[33;34m |*                                           *| \033[33;0m"
-defaults write -g com.apple.mouse.scaling 3 &> /dev/null
-echo "\033[33;34m |*            - Max speed mouse       OK [✓] *| \033[33;0m"
-echo "\033[33;34m |*                                           *| \033[33;0m"
-defaults write com.apple.mouse enableSecondaryClick 1 &> /dev/null
-echo "\033[33;34m |*            - On secondary click    OK [✓] *| \033[33;0m"
-echo "\033[33;34m |*                                           *| \033[33;0m"
-echo "\033[33;34m |*       =======  COMPLETED 😎  =====         *| \033[33;0m"
-echo "\033[33;34m |*                                           *| \033[33;0m"
-echo "\033[33;34m |*********************************************| \033[33;0m \n"
+# Color setting
+C_WHITE="\033[33;0m";
+C_BLACK="\033[33;30m";
+C_BLUE="\033[33;34m";
+C_YELLOW="\033[33;33m";
+C_GREEN="\033[33;32m";
+C_RED="\033[33;91m";
+C_PURPLE="\033[33;35m";
+
+# Template setting
+C_TMP=${C_BLUE};
+C_DRAW=1
+
+# Script Template
+T_TITLE="42 mac conf by Afxsoft"
+
+function	echox()
+{
+	line="----------"
+
+	while test $# -gt 0
+	do
+		line+=" $1 "
+		shift
+	done
+	echo ${line} ${C_TMP};
+}
+
+echo ${C_TMP}
+echo "          ******************************        "
+echo "               42 mac conf by Afxsoft           "
+echo "          ******************************        "
+
+echo ""
+curl -L http://install.ohmyz.shi &> /dev/null
+if [ $? == 0 ]; then
+		curl -L http://install.ohmyz.sh &> /dev/null | sh &> /dev/null
+		echox ${C_WHITE} "- Oh my Zsh" ${C_GREEN} "OK [✓] "
+	else
+		echox ${C_WHITE} "- Oh my Zsh" ${C_RED} "FAIL [X]"
+fi
+echo ""
+sed -i.bu 's/ZSH_THEME="robbyrussell"/ZSH_THEME="af-magic"/' ~/.zshrc &&
+echox ${C_WHITE} "- Theme af-magic"    ${C_GREEN} "OK [✓]"
+echo ""
+curl -L https://raw.githubusercontent.com/reversTeam/Sublivim/master/installer.sh &> /dev/null | sh &> /dev/null &&
+echox ${C_WHITE} "- Sublivim" ${C_GREEN} "OK [✓]"
+echo ""
+rm -rf /tmp/vim_theme/ &&
+git clone https://github.com/chriskempson/vim-tomorrow-theme.git /tmp/vim_theme &> /dev/null &&
+cp /tmp/vim_theme/colors/Tomorrow-Night.vim ~/.vim/colors/. &&
+sed -i.bu 's/colorscheme molokai/colorscheme Tomorrow-Night/' ~/.vimrc &&
+echox ${C_WHITE} "- Theme Tomorrow-Night" ${C_GREEN} "OK [✓]"
+echo ""
+defaults write -g com.apple.mouse.scaling 3 &> /dev/null &&
+echox ${C_WHITE} "- Max speed mouse" ${C_GREEN} "OK [✓]"
+echo ""
+defaults write com.apple.mouse enableSecondaryClick 1 &> /dev/null &&
+echox ${C_WHITE} "- On secondary click" ${C_GREEN} "OK [✓]"
+echo ""
+defaults -currentHost write com.apple.screensaver askForPassword -int 1  &> /dev/null &&
+echox ${C_WHITE} "- Passeword require" ${C_GREEN} " OK [✓]"
+echo ""
+#open -n /Applications/iTerm\ 2.app && exit
+echox ${C_WHITE} " TODO" ${C_GREEN} "OK [✓]"
+echo ""
+echo "            =======  COMPLETED 😎  =====         "
+echo "             https://github.com/Afxsoft"
+echo "            ***************************         "
